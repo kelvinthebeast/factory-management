3bedb1dc9949c44b7cb7ec94961806955b7738fc
le thanh nhan <thanhnhan13082021@gmail.com>
2024-09-03 20:31:16 +0700

    Config controllers folder done

--- routes/client/home.route.js
+++ routes/client/home.route.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const controller = require("../../controllers/client/home.controller");
 
 
-
-
-    router.get("/",(req, res)=> {
-        res.render("client/pages/home/index.pug");
-    })
+router.get("/", controller.index)
 module.exports = router;
