9e08601f15520af5e270032ff48ef5487a66e05a
le thanh nhan <thanhnhan13082021@gmail.com>
2024-09-04 19:39:14 +0700

    config pug folder

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,9 @@
 module.exports.index = (req, res)=> {
-    res.render("client/pages/home/index.pug");
+    res.render("client/pages/home/index.pug", {
+        pageTitle: "Trang chủ"
+
+    });
+    
 }
 
 
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,3 +1,7 @@
 module.exports.index = (req, res)=> {
-    res.render("client/pages/products/index")
+    res.render("client/pages/products/index",{
+        pageTitle: "Trang danh sách sản phẩm"
+
+    })
+    
 };
