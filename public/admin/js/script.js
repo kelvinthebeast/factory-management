d0ef56523d73d02f2083db11f0db2508fa63a3e1
le thanh nhan <thanhnhan13082021@gmail.com>
2024-09-11 11:06:37 +0700

    đã làm xong trang tính năng lọc sản phẩm hiện thực trên url được rồi

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -2,11 +2,25 @@
 
 const buttonStatus = document.querySelectorAll("[button-status]");
 // console.log(buttonStatus);
+
+let url = new URL(window.location.href);
 if(buttonStatus.length > 0) {
+    
     buttonStatus.forEach(button => {
         button.addEventListener('click', ()=> {
             const status = button.getAttribute("button-status");
-            console.log(status);
+            // console.log(status);
+
+            if(status) {
+                url.searchParams.set("status", status);
+
+            } else {
+                url.searchParams.delete("status");
+            }
+            // console.log(url.href);
+
+            // chuyển hướng lên thanh search của win dow
+            window.location.href = url.href
         })
     })
 
