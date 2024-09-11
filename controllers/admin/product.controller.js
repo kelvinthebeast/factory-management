e39c3b807e3b76c7a06a8f80a168eaeef1a3363b
le thanh nhan <thanhnhan13082021@gmail.com>
2024-09-11 10:17:23 +0700

    Đã thêm duduoc loc trạng thái dùng params ttren thanh search

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,13 +1,23 @@
 // [GET] /admin/products
 
+const { query } = require("express");
 const Product = require("../../models/product.model");
 module.exports.index = async (req, res) => {
 
-    const products = await Product.find({
-        deleted:false
-    });
+    // console.log(req.query);
+    let find = {
+        deleted: false
+    }
 
+    if(req.query.status){
+        find.status = req.query.status;
+
+    }
+    const products = await Product.find(find);
+    console.log("ĐÂY LÀ SẢN PHẨM ĐANG TÌM");
     console.log(products);
+
+    // console.log(products);
 
 
     res.render("admin/pages/products/index",{
