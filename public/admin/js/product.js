489b164eba66b063b2ce9f8f329a4c1199e4df00
le thanh nhan <thanhnhan13082021@gmail.com>
2024-09-29 14:54:58 +0700

    XÓa vĩnh viễn done

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -31,3 +31,25 @@ if (buttonChangeStatus.length > 0) {
 
 }
 // end change status
+
+// button delete
+const buttonDelete = document.querySelectorAll("[button-delete]");
+if(buttonDelete.length > 0) {
+    const formDeleteItem = document.querySelector("#form-delete-item");
+    const path = formDeleteItem.getAttribute("data-path");
+    buttonDelete.forEach(button => {
+        button.addEventListener("click", ()=> {
+
+            const isComfirm = confirm("bạn có chắc muốn xóa sản phẩm không");
+            if(isComfirm) {
+                const id = button.getAttribute("data-id");
+                
+                const action = `${path}/${id}?_method=DELETE`;
+                console.log(action);
+                formDeleteItem.action = action;
+                formDeleteItem.submit();
+            }
+        })
+    })
+}
+//button delete
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -10,4 +10,6 @@ router.patch("/change-status/:status/:id", controller.changeStatus);
 
 
 router.patch("/change-multi", controller.changeMulti);
+
+router.delete("/delete/:id", controller.deleteItem);
 module.exports = router;
