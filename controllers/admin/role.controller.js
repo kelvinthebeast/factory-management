9e486b4eda7bd23d59d5f992ed664a794d54d7bb
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2024-11-18 09:46:28 +0700

    edit using patch get for role

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -23,11 +23,11 @@ module.exports.index = async (req, res) => {
 
 module.exports.create = async (req, res) => {
 
-    
+
     res.render("admin/pages/roles/create", {
 
         pageTitle: "Tạo mới mục quyền",
-     
+
 
     });
 }
@@ -35,12 +35,42 @@ module.exports.create = async (req, res) => {
 // Post admin/pages/roles/create
 module.exports.createPost = async (req, res) => {
 
-   
-   
+
+
 
     const record = new Role(req.body);
 
     await record.save();
 
     res.redirect(`${systemConfig.prefixAdmin}/roles`);
+}
+// GET admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+        let find = {
+            deleted: false,
+            _id: id
+        }
+
+        const data = await Role.findOne(find);
+        res.render("admin/pages/roles/edit", {
+            pageTitle: "Sửa mục quyền",
+            data: data
+        })
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/admin/roles`)
+    }
+
+}
+
+
+// PATCH admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+    await Role.updateOne({ _id: id }, req.body);
+    req.flash("success","Cập nhật thành công");
+    res.redirect(`back`)
+
+
 }
