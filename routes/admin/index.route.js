d181b0def1950f0f2aae1a010479bb929f018002
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2024-11-25 21:23:47 +0700

    tính năng đăng nhập

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,6 +4,8 @@ const productRoutes = require("./product.route");
 const productCategoryRoutes = require("./product-category.route");
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
+const authRoutes = require("./auth.route");
+
 
 module.exports = (app) => {
     // app.use("/", homeRoutes);
@@ -17,6 +19,8 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + `/products-category`, productCategoryRoutes);
     app.use(PATH_ADMIN + `/roles`, roleRoutes);
     app.use(PATH_ADMIN + `/accounts`, accountRoutes);
+
+    app.use(PATH_ADMIN + `/auth`, authRoutes)
 
 
 
