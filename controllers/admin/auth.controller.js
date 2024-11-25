1d28835b8c46228d3446d0ae41e9dbe0040a3dd6
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2024-11-25 23:57:21 +0700

    add page logout and done

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -40,3 +40,10 @@ module.exports.loginPost = async (req, res) => {
     res.cookie("token",user.token)
     res.redirect(`${systemConfig.prefixAdmin}/dashboard`)
 }
+
+module.exports.logout = (req, res) => {
+    // xóa token có trong login 
+    res.clearCookie("token")
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
+ 
+}
