c896f8f1c24eb19957a6f4a6958cc628812ba8ed
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2024-12-02 14:05:36 +0700

    fix permission phân quyền

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -59,24 +59,15 @@ module.exports.create = async (req, res) => {
 
 
 
-//POST /admin/product-category/create
-// module.exports.createPost = async (req, res) => {
-//     if(req.body.postiton =="") {
-//         const count = await ProductCategory.countDocuments();
-//         req.body.position = count + 1;
-//     } else {
-//         req.body.position = parseInt(req.body.position);
-//     }
-//     const record = new ProductCategory(req.body);
-//     await record.save();
-//     // res.redirect(`${systemConfig.prefixAdmin}/products-category`);
-//     res.redirect(`${systemConfig.prefixAdmin}/products-category`);
-// };
-
-
 // POST /admin/product-category/create
 module.exports.createPost = async (req, res) => {
     try {
+
+        // const permissions =res.locals.role.permissions
+        //if(permissions.includes("product-category_create")) {
+        // console.log(có quyển)
+        // } else {
+        // return}
         // If `position` is empty, set it to the current count + 1
         if (req.body.position === "" || req.body.position === undefined) {
             const count = await ProductCategory.countDocuments();
