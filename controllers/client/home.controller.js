393ca5d31f09c6a4205d8f9e6a5d10b472c77814
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2024-12-04 11:19:35 +0700

    push origin main for err

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,11 +1,23 @@
+const Product = require("../../models/product.model");
+
+
 // [GET] /
 
 
 module.exports.index = async (req, res)=> {
-    
+    // const productsFeatured =  await Product.find({
+    //     featured: "1",
+    //     deleted: false,
+    //     status:"active"
 
+    // })
+    // const newProducts = productsFeatured.map(item => {
+    //     item.priceNew = (item.price*(100 - item.discountPercentage)/100).toFixed(0);
+    //     return item;
+    // })
     res.render("client/pages/home/index.pug", {
         pageTitle: "Trang chủ",
+        // productsFeatured: newProducts
         
     });
     
