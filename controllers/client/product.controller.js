3c90ff7cb95cb2c8df4512db939923ad0595edae
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2024-12-05 09:07:32 +0700

    raw push

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,7 @@
 const Product = require("../../models/product.model");
 const productsHelper = require("../../helpers/products");
+
+const ProductCategory = require("../../models/product-category.model");
 // GET /products
 module.exports.index = async (req, res)=> {
     const products = await Product.find({
@@ -18,24 +20,40 @@ module.exports.index = async (req, res)=> {
 };
 
 
-// [GET] /products/:slug
-module.exports.detail = async (req, res) =>{
-    try {
-        const find = {
-            deleted: false,
-            slug: req.params.slug,
-            status: "active"
-        }
+// // [GET] /products/:slug
+// module.exports.detail = async (req, res) =>{
+//     try {
+//         const find = {
+//             deleted: false,
+//             slug: req.params.slug,
+//             status: "active"
+//         }
     
-        const product = await Product.findOne(find);
-        console.log(product)
-        res.render("client/pages/products/detail", {
-            pageTitle: product.title,
-            product: product
-        });
-    } catch (error) {
-        req.flash("error", "Không tìm thấy sản phẩm này");
-        res.redirect(`/products`);
+//         const product = await Product.findOne(find);
+//         console.log(product)
+//         res.render("client/pages/products/detail", {
+//             pageTitle: product.title,
+//             product: product
+//         });
+//     } catch (error) {
+//         req.flash("error", "Không tìm thấy sản phẩm này");
+//         res.redirect(`/products`);
         
-    }
+//     }
+// }
+// [Get] /products/:slugCategory
+module.exports.category = async (req, res)=> {
+    console.log(req.params.slugCategory);
+    const category = await ProductCategory.findOne({ slug: req.params.slugCategory, deleted: false});
+
+    const products = await ProductCategory.find({
+        product_category_id: category.id,
+        deleted: false
+    }).sort({position: "desc"});
+    // res.send("OKE")
+    const newProducts = productsHelper.priceNewProducts(products);
+    res.render("client/pages/products/index", {
+        pageTitle: category.title,
+        products: newProducts
+    })
 }
