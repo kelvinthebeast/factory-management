dba8f910a7b3176d88ea76c269f08c1b95f570f0
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2024-12-13 10:52:34 +0700

    search-page using regex

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,12 +1,15 @@
 const categoryMiddleware = require("../../middlewares/client/category.middleware")
 const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
+const searchRoutes = require("./search.route");
 module.exports = (app) => {
     // app.use("/", homeRoutes);
     // luôn luôn có middleware này
     app.use(categoryMiddleware.category);
     app.use("/",homeRoutes);
     app.use("/products",productRoutes);
+
+    app.use("/search", searchRoutes);
 
    
 }
