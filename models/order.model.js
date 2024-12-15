43ba8b14c2007d4b5cf36464dd59ff38e3bd5a01
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2024-12-15 21:31:30 +0700

    add order model

---
+++ models/order.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    title: String,
+    // user_id: String,
+    cart_id: String,
+    description: String,
+    userInfo: {
+        fullName: String,
+        email: String,
+        phone: String,
+        address: String
+    },
+    product:[
+        {
+            product_id: String,
+            quantity: Number,
+            discountPercentage: Number,
+            quantity: Number
+        }
+    ],
+    permissions: {
+        type: Array,
+        default: []
+    },
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: Date,
+    timestamps: true
+});
+
+const Order = mongoose.model("Order", orderSchema, "orders");
+
+module.exports = Order;
