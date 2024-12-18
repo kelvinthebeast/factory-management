74e27e457ea30358e6666ea0633b12d69e6c7ed2
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2024-12-18 14:00:20 +0700

    add logout funtionality

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -84,3 +84,10 @@ module.exports.loginPost = async (req, res) => {
     res.cookie("tokenUser", user.tokenUser);
     res.redirect("/");
 };
+
+// [get] /user/logout 
+module.exports.logout = (req, res) => {
+    res.clearCookie("tokenUser");
+
+    res.redirect("/user/login");
+};
