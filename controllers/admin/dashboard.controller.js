dc3696564988070bf66a32acd1ad5da34776544b
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2024-12-22 14:06:41 +0700

    user.add dashboard page client

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,9 +1,40 @@
 //[GET] /admin/dashboard
+const ProductCategory = require("../../models/product-category.model");
 
-module.exports.dashboard = (req, res) => {
+// [GET] /admin/dashboard
+module.exports.dashboard = async (req, res) => {
+    const statistic = {
+        categoryProduct: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+        product: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+        account: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+        user: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+    };
+
+    statistic.ProductCategory.total = await ProductCategory.countDocuments({deleted: false});
+    statistic.productCategory.active = await ProductCategory.countDocuments({deleted: false, status: true});
+    statistic.productCategory.inactive = await ProductCategory.countDocuments({deleted: false, status: false});
     res.render("admin/pages/dashboard/index.pug", {
-        pageTitle: "Trang chủ"
+        pageTitle: "Trang chủ",
+        statistic: statistic
 
     });
 
-}
+};
+
+    
